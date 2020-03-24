e73b80c85d666aaf16047b4838b3c3eda28b02d0
Nikita Dyshkant <nikitadyshkant@gmail.com>
2020-03-24 20:53:24 +0300

    initial

---
+++ 1.py
@@ -0,0 +1,76 @@
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
+
+# Каталог с данными для обучения
+train_dir = 'train'
+# Каталог с данными для проверки
+val_dir = 'test'
+# Каталог с данными для тестирования
+test_dir = 'val'
+# Размеры изображения
+img_width, img_height = 150, 150
+# Размерность тензора на основе изображения для входных данных в нейронную сеть
+# backend Tensorflow, channels_last
+input_shape = (img_width, img_height, 3)
+# Количество эпох
+epochs = 30
+# Размер мини-выборки
+batch_size = 16
+# Количество изображений для обучения
+nb_train_samples = 2700
+# Количество изображений для проверки
+nb_validation_samples = 300
+# Количество изображений для тестирования
+nb_test_samples = 6
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+train_generator = datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+val_generator = datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+test_generator = datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=val_generator,
+    validation_steps=nb_validation_samples // batch_size)
+scores = model.evaluate_generator(test_generator, nb_test_samples // batch_size)
+print("Аккуратность на тестовых данных: %.2f%%" % (scores[1]*100))
